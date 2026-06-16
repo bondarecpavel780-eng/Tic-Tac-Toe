a42127460152a5985899917e31461fbe3f7315c1
Pavel <bondarecpavel780@gmail.com>
2026-06-16 21:20:30 +0300

    Completion of the Tic Tac Toe project with full functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react"
 
-export default function Player({initialName, symbol}) {
-const [playerName, setPlayerName] = useState(initialName)
+export default function Player({initialName, symbol, isActive, onChangeName}) {
+const [playerName, setPlayerName] = useState(initialName);
 const [isEditing, setIsEditing] = useState(false);
 function editName() {
     setIsEditing((editing) => !editing);
-
+    if(isEditing){
+    onChangeName(symbol, playerName);
+    }
 }
 
 function handleChange(event){
@@ -19,7 +21,7 @@ if(isEditing){
 
 } 
     return (
-      <li>
+      <li className={isActive ? 'active' : undefined}>  
             <span className="player">
                 {playerInfo}
                 <span className="player-symbol">{symbol}</span>
